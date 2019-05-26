28359fef8aa4777822ed6fa1802f211e46e78103
mikep252 <mikep252@wp.pl>
2019-05-26 19:11:44 +0200

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -149,29 +149,3 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #35495e;
-}
-
-.date-input {
-  float: none;
-  margin: 0 auto;
-}
-</style>
