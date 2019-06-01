bce55e39ed9205287a3f5160c08a889fb3d479d5
AlicjaBank <44546742+AlicjaBank@users.noreply.github.com>
2019-06-02 00:53:25 +0200

    style

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -211,3 +211,33 @@ export default {
 }
 </script>
 
+<style>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495e;
+}
+
+.middle {
+  float: none;
+  margin: 0 auto;
+}
+
+.map {
+  width: 600px;
+  height: 600px;
+}
+</style>
